c791accf93a174e1bcf35a4272669405cb9ca47b
DIT <pratsu20@gmail.com>
2026-06-17 12:52:46 +0530

    manual CI/CD trigger for smoke tests

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,19 +4,31 @@ import driver.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
+    protected static final String BASE_URL = "https://ditinteractive.com/";
+
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://ditinteractive.com/");
+        ChromeOptions options = new ChromeOptions();
 
+        String headless = System.getProperty("headless");
+        if ("true".equalsIgnoreCase(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
         DriverFactory.setDriver(driver);
     }
 
